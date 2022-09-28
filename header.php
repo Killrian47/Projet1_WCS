f7c18bbea059f27b2e2557082553b31419e47daa
Jordan Lassalle <lassalle.jordan@gmail.com>
2022-09-28 14:44:23 +0200

    Set up CSS and HTML files with imports

--- header.php
+++ header.php
@@ -5,7 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="assets/header.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     
 
@@ -44,23 +43,6 @@
             </div>
         </nav>
     
-    </header>
-
-    <main>
-        <section class="francais">
-            Salut je suis pointilleux sur les bords !
-        </section>
-
-        <section class="anglais">
-            Hi, I'm picky around the edges!  
-        </section>
-    </main>
-
-    <footer>
-        <p class="footer-copy">@copyright 2022 Me and them !</p>
-    </footer>
-
-    <script src="script.js"></script>
-    
+    </header>  
 </body>
 </html>
