e4b32536f570b03c4a23de8f9c771761b6c86302
salut <killian.portier@yahoo.fr>
2022-10-03 11:14:39 +0200

    add footer

---
+++ footer.php
@@ -0,0 +1,12 @@
+    
+    <footer>
+        <div class="footer_bis">
+
+            <p>&copy; 2022 all right reserved</p>
+            <a href="#"><i class="fas fa-arrow-up up"></i></a>
+        </div>
+        <script src="script.js"></script>
+    </footer>
+    
+    </body>
+</html>
